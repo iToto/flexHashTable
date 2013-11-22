487a539b396da044ee41c6f5a4cbeafadd707161
sandrodago <dagostino.alex@gmail.com>
2013-11-21 20:35:36 -0500

    added a prime finding class
    the class finds the second largest prime number from a given number

---
+++ asst3/src/DoubleHashPrime.java
@@ -0,0 +1,37 @@
+class DoubleHashPrime	 {
+
+	public DoubleHashPrime(){
+		private int perfectPrime=0;
+		private int perfectPrimeCounter=3;
+	}
+	
+	public static findPerfectPrime(int num)
+	{
+		for( int i=num; i>=2; i--)
+		{
+			if ( perfectPrime(i) )
+			{
+				perfectPrimeCounter++;
+				perfectPrime=i;
+				if (perfectPrimeCounter == 2)
+				{
+					return perfectPrime;
+				}
+			}
+		}
+		return perfectPrime;
+
+	}
+
+	private static perfectPrime(int num)
+	{
+		int count = 0;
+		for(int i=2; i<num; i++)
+		{
+			if (num%i == 0)
+				return false;
+		}
+		return true;
+	}
+
+}
